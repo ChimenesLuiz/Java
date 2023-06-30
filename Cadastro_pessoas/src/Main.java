bfc67843d0f4e66673aafb78b6005c696635fdf2
luiz <luizguichimenes2002@gmail.com>
2023-06-30 16:46:38 -0400

    terminei

--- Cadastro_pessoas/src/Main.java
+++ Cadastro_pessoas/src/Main.java
@@ -3,16 +3,19 @@ import java.util.Scanner;
 import java.util.LinkedHashMap;
 class Main{
     public static void main(String[] wxyz){
-        Pessoa objeto = new Pessoa();
-        Scanner input = new Scanner(System.in);
+
+        Pessoa objeto = new Pessoa(); //responsavel pela interacao com o BD
+        Scanner input = new Scanner(System.in); //responsavel pelos inputs do usuario
         LinkedHashMap<String, String> dados = new LinkedHashMap<>();
+        String novo_email = "";
         while (1==1){
+            //menu principa;
             System.out.println("-----------------------------------");
-            System.out.println("[1] - Cadastrar Pessoa \n[2] - Consultar Cadastro \n[0] - Sair");
+            System.out.println("[1] - Cadastrar Pessoa \n[2] - Editar dados\n[0] - Sair");
             System.out.print("Escolha: ");
             int escolha = input.nextInt();
 
-            if (escolha == 1){
+            if (escolha == 1){//logica para cadastro no BD
                 System.out.print("Nome: ");
                 dados.put("nome", input.next());
                 input.nextLine();
@@ -25,27 +28,53 @@ class Main{
                 dados.put("senha", input.next());
                 input.nextLine();
 
+                //chamando o metodo de cadastro
                 objeto.cadastroPessoa(dados.get("nome"), dados.get("email"), dados.get("senha"));
-                dados.clear();
+                dados.clear(); //zerando o dicionario dados
             }
 
-            else if (escolha == 2){
+            else if (escolha == 2){ //logica para editar dados no BD
+                //coletando email
                 System.out.print("Digite seu email: ");
                 String email = input.next();
                 input.nextLine();
-                dados = objeto.consultarDados(email);
+                //buscando no BD por email
+                dados = objeto.getDados(email);
                 System.out.println("-----------------------------------");
+                int c = 0;
+                //for usado para imprimir na tela
                 for (Map.Entry<String, String> entry : dados.entrySet()){
                     String chave = entry.getKey();
                     String valor = entry.getValue();
-                    System.out.println(chave + ": " + valor);
+                    System.out.println("[" + (c+1) + "]" + " - " + chave + ": " + valor);
+                    c += 1;
                 }
-                dados.clear();
+                dados.clear(); //zerando o dicionario dados
+                System.out.print("Qual campo deseja editar: ");
+                escolha = input.nextInt();
+                //switch usado para chamar algum metodo e realizar alguma edicao
+                switch (escolha){
+                    case 1:
+                        System.out.print("Digite o novo nome: ");
+                        String novo_nome = input.next();
+                        objeto.setNome(novo_nome, dados.get("email"));
+                        break;
+                    case 2:
+                        System.out.print("Digite o novo email: ");
+                        novo_email = input.next();
+                        objeto.setEmail(dados.get("email"), novo_email);
+                        break;
+                    case 3:
+                        System.out.print("Digite a nova senha: ");
+                        String nova_senha = input.next();
+                        objeto.setSenha(nova_senha, novo_email);
+                        break;
+                }
+                dados.clear(); //zerando dicionario dados
             }
-            else if (escolha == 0){
+            else if (escolha == 0){ //interrompe o loop while
                 break;
             }
-
         }//while
     }//main
 }//clas
--- Cadastro_pessoas/src/Pessoa.java
+++ Cadastro_pessoas/src/Pessoa.java
@@ -2,15 +2,17 @@ import java.sql.*;
 import java.util.LinkedHashMap;
 
 public class Pessoa{
+    //declarando os atributos da classe
     private final String url, user, password;
     private Connection conexao;
     public Pessoa(){
-        this.url = "jdbc:mysql://localhost:3306/GrupoCard";
-        this.user = "root";
-        this.password = "@D414n45376491";
+        //preenchendo informacoes relativas ao BD (sao exemplos)
+        this.url = "exemplo:jdbc:mysql://localhost:3306/GrupoCard";
+        this.user = "seu_usuario";
+        this.password = "sua_senha";
         this.conexao = null;
     }
-    private void conectar(){
+    private void conectar(){//funcao para conectar com o BD
         try{
             this.conexao = DriverManager.getConnection(this.url, this.user, this.password);
         } catch (SQLException e){
@@ -19,34 +21,35 @@ public class Pessoa{
     }
     public void cadastroPessoa(String nome, String email, String senha){
         try{
-            conectar();
-            String comando = "INSERT INTO Pessoa(nome, email, senha) VALUES(?, ?, ?)";
-            PreparedStatement statement = conexao.prepareStatement(comando);
+            conectar(); //conectando com o BD
+            String comando_sql = "INSERT INTO Pessoa(nome, email, senha) VALUES(?, ?, ?)";
+            PreparedStatement statement = conexao.prepareStatement(comando_sql); //preparando o comando SQL
+            //substituindo as interrogacoes do "comando_sql" sendo (1 = indice e {nome} = valor)
             statement.setString(1, nome);
             statement.setString(2, email);
             statement.setString(3, senha);
-            int linhasAfetadas = statement.executeUpdate();
+            //encerrando conexao
             statement.close();
             conexao.close();
-            System.out.println("Linhas afetadas: " + linhasAfetadas);
         } catch (SQLException e){
             e.printStackTrace();
         }
     }
-    public LinkedHashMap consultarDados(String email){
-        LinkedHashMap<String, String> dados = new LinkedHashMap<String, String>();
+    public LinkedHashMap getDados(String email){
+        LinkedHashMap<String, String> dados = new LinkedHashMap<String, String>(); //dicionario usado para coletar dados
         try{
             conectar();
             String comandoSql = "SELECT * FROM Pessoa WHERE email = ?";
             PreparedStatement statement = conexao.prepareStatement(comandoSql);
             statement.setString(1, email);
-            ResultSet resultado = statement.executeQuery();
+            ResultSet resultado = statement.executeQuery(); //armazenando o resultado do SELECT
 
-            while (resultado.next()){
+            while (resultado.next()){ //percorrendo o resultado e atribuindo ao dicionario
                 dados.put("nome", resultado.getString("nome"));
                 dados.put("email", resultado.getString("email"));
                 dados.put("senha", resultado.getString("senha"));
             }
+            //encerrando conexao
             resultado.close();
             statement.close();
             conexao.close();
@@ -54,6 +57,55 @@ public class Pessoa{
         } catch (SQLException e){
             e.printStackTrace();
         }
-        return dados;
+        return dados; //retornando o dicionario
+    }
+
+    public void setNome(String nome, String email){
+        try{
+            conectar();
+            String comando_sql = "UPDATE Pessoa SET nome = ? WHERE email = ?";
+            PreparedStatement statement = conexao.prepareStatement(comando_sql);
+            statement.setString(1, nome);
+            statement.setString(2, email);
+            statement.executeUpdate();
+
+            statement.close();
+            conexao.close();
+            System.out.println("-----------------------------------");
+            System.out.println("Dados alterados com sucesso! ");
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+    public void setEmail(String email, String novo_email){
+        try{
+            conectar();
+            String comando_sql = "UPDATE Pessoa SET email = ? where email = ?";
+            PreparedStatement statement = conexao.prepareStatement(comando_sql);
+            statement.setString(1, novo_email);
+            statement.setString(2, email);
+            statement.executeUpdate();
+
+            conexao.close();
+            statement.close();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+    public void setSenha(String senha, String email){
+        try{
+            conectar();
+            String comando_sql = "UPDATE Pessoa SET senha = ? WHERE email = ?";
+            PreparedStatement statement = conexao.prepareStatement(comando_sql);
+            statement.setString(1, senha);
+            statement.setString(2, email);
+            statement.executeUpdate();
+            conexao.close();
+            statement.close();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 }
+
+
